e38e7d9c446351d965eab48c90343e2d68b365fa
Cesar Cisnero <cesarlgi01@gmail.com>
2024-10-24 17:26:29 -0300

    Fix

--- src/app/shared/components/layouts/sidebar/sidebar-menu.config.ts
+++ src/app/shared/components/layouts/sidebar/sidebar-menu.config.ts
@@ -2,9 +2,11 @@ export const SIDEBAR_MENU_CONFIG = [
     {
         name: 'Posts',
         route: 'posts',
+        icon: 'pi pi-inbox',
     },
     {
         name: 'Users',
         route: 'users',
+        icon: 'pi pi-user',
     },
 ]
